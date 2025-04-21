50a07827b8c16f7738f0fae967fff8d9f47028e9
WahabSyahranie <wahabschools@gmail.com>
2025-04-21 15:24:48 +0800

    episode 3 - struktur folder

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Wahab Syahranie']);
+});
