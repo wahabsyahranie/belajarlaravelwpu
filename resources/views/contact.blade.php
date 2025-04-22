35c0c49afd11ec463acc242d3c7af8c0371f5f6e
WahabSyahranie <wahabschools@gmail.com>
2025-04-22 09:42:36 +0800

    install tailwind v4 (v3 error mulu)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,15 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>About</title>
-  <link rel="stylesheet" href="css/style.css">
+  <title>Halaman Contact</title>
 </head>
 <body>
   <a href="/home">Home</a>
+  <a href="/about">About</a>
   <a href="/blog">Blog</a>
-  <a href="/contact">Contact</a>
-  <h3>Owner : {{ $nama }}</h3>
-  <img src="img/person.png" alt="person" width="200">
-  <script src="js/script.js"></script>
+  <h1>Halaman Contact</h1>
+  <ul>
+    <li>
+      <a href="https://instagram.com/wahabsyahranie">Instagram</a>
+    </li>
+    <li>
+      <a href="https://youtube.com/wahabsyahranie">Youtube</a>
+    </li>
+  </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@ Route::get('/home', function () {
 Route::get('/about', function () {
     return view('about', ['nama' => 'Wahab Syahranie']);
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
